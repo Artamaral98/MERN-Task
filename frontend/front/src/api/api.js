b57f05ab697703c6289968df39fbe9fb03982226
Arthur <arthuramaral1509@gmail.com>
2024-11-06 20:10:31 -0300

    alteração3

--- frontend/front/src/api/api.js
+++ frontend/front/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://mern-task-deploy-alpha.vercel.app/api'
 })
 
 export default api
