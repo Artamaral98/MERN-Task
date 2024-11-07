6cfd76debda2b5a07c0f0799a82508aefb167302
Artamaral98 <167587561+Artamaral98@users.noreply.github.com>
2024-11-07 16:43:26 -0300

    Update api.js

--- frontend/front/src/api/api.js
+++ frontend/front/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://mern-task-6wmv.onrender.com/'
+    baseURL: 'https://mern-task-6wmv.onrender.com'
 })
 
 export default api
