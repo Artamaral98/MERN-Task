4824f440a8cf5ade182b5c7dac022ff2019eaade
Artamaral98 <167587561+Artamaral98@users.noreply.github.com>
2024-11-18 17:59:24 -0300

    Update taskModel.js teste

--- backend/src/models/taskModel.js
+++ backend/src/models/taskModel.js
@@ -3,7 +3,7 @@ import mongooseSequence from "mongoose-sequence"
 
 
 const TaskSchema = new mongoose.Schema({
-    _id: Number,
+
     
     name: {
         type: String,
@@ -26,6 +26,6 @@ const TaskSchema = new mongoose.Schema({
 {timestamps: true})
 
 TaskSchema.plugin(mongooseSequence(mongoose), {inc_field: 'order'})
-TaskSchema.plugin(mongooseSequence(mongoose), {inc_field: '_id'})
+
 
 export const Task = mongoose.model("tasks", TaskSchema)
