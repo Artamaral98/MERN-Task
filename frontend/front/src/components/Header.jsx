c9adf5e838d7bce2ebb1124cd58fbf47dee5638b
Artamaral98 <167587561+Artamaral98@users.noreply.github.com>
2024-11-18 17:32:26 -0300

    Update Header.jsx

--- frontend/front/src/components/Header.jsx
+++ frontend/front/src/components/Header.jsx
@@ -1,6 +1,7 @@
 const Header = () => {
     return (
         <tr className="bg-teal-950 text-white">
+            <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Id</th>
             <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Tarefa</th>
             <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Prazo</th>
             <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Custo</th>
