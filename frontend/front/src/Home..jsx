dd45e8a15061202f8a9f5a783afec0c918f72c63
Artamaral98 <167587561+Artamaral98@users.noreply.github.com>
2024-11-19 19:19:33 -0300

    Update Home..jsx

--- frontend/front/src/Home..jsx
+++ frontend/front/src/Home..jsx
@@ -48,6 +48,7 @@ const App = () => {
                   setTasks={setTasks}
                   handleEdit={handleEdit}
                   openDeleteModal={openDeleteModal}
+                  handleReorderTasks={handleReorderTasks}
                 />
               ))}
             </tbody>
